d99dddbb62db5f695c1ec6a488750067e84c1c6d
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-25 21:52:41 -0600

    Add Map facade

---
+++ spec/facades/map_facade_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe MapFacade do
+  describe '#map_facade' do
+    it 'returns latitude and longitude of a searched city', :vcr do
+
+      coors = MapFacade.lat_long('denver, co')
+
+      expect(coors.first).to eq(39.738453)
+      expect(coors.last).to have_key(-104.984853)
+    end
+  end
+end
