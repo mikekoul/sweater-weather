b55676c45ebf8bd4deb3cdcf4dad39f1dc6d5040
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-26 11:47:51 -0600

    Final Commit

--- app/serializers/serarch_serializer.rb
+++ app/serializers/serarch_serializer.rb
@@ -1,18 +1,23 @@
 class SearchSerializer
   include JSONAPI::Serializer
 
-  
+# needs initializer
+  def initialize(response_data)
+  end
+
   def response
     {
       "data": {
         "id": "null",
         "type": "books",
         "attributes": {
+          #not sure where to get the destination from
           "destination": [params[:location]],
           "forecast": {
             "summary": @cw.condition,
             "temperature": @cw.temperature
           },
+          #not sure how to pull the numfound data here
           "total_books_found": data[:numFound],
           "books": [
           {
