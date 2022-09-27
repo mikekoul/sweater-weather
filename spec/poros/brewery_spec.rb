c761178f77c894068fc31afe9fa92dcbece3a4d7
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-27 09:42:36 -0600

    test/Brewery poros

--- spec/poros/brewery_spec.rb
+++ spec/poros/brewery_spec.rb
@@ -2,9 +2,22 @@ require 'rails_helper'
 
 RSpec.describe Brewery do
   describe '#attributes' do
-    it 'return breweries in a given location', :vcr do
-
+    it 'has attributes', :vcr do
       
+      data = {
+        id: "10129",
+        name: "Denver Beer Co Olde Town Arvada",
+        brewery_type: "micro",
+        city: "denver"
+      }
+
+      results = Brewery.new(data)
+
+      expect(results).to be_a(Brewery)
+      expect(results.id).to eq("10129")
+      expect(results.name).to eq("Denver Beer Co Olde Town Arvada")
+      expect(results.brewery_type).to eq("micro")
+      expect(results.city).to eq("denver")
     end
   end
 end
