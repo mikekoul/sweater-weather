7e0e1ed2b55634b49b7a686db8ef53ce0c3aadb3
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-27 09:46:31 -0600

    Test passed for brewery poros

--- app/poros/brewery.rb
+++ app/poros/brewery.rb
@@ -1,3 +1,11 @@
 class Brewery
-  
+
+  attr_reader :id, :name, :brewery_type, :city
+
+  def initialize(data)
+    @id = data[:id]
+    @name = data[:name]
+    @brewery_type = data[:brewery_type]
+    @city = data[:city]
+  end
 end
