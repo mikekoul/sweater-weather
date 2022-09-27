57939d38f278bd694c066a47165fa15214fe5bee
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-27 14:46:49 -0600

    feat/Add feature to create a new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-    user = User.new(email: user_params[:email], password: user_params[:password], password_confirmation: user_params['password_confirmation'], api_key: SecureRandom.hex)
+    user = User.new(email: user_params[:email], password: user_params[:password], password_confirmation: user_params[:password_confirmation], api_key: SecureRandom.hex)
     if user.save
-      render json: UserSerialzer.new(user), status: 201
+      render json: UserSerializer.new(user).response, status: 201
     else
       render json: { data: {} }, status: 400
     end
   end
-
+  
   private
-    def user_params
+  def user_params
       params.permit(:email, :password, :password_confirmation)
     end
 end
--- spec/requests/api/v1/user_account_creation_spec.rb
+++ spec/requests/api/v1/user_account_creation_spec.rb
@@ -8,11 +8,25 @@ describe 'User Account Creation API' do
       "password_confirmation": 'pass123'
     }
 
-    post '/api/v1/users', params: user_info
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/users', headers: headers, params: JSON.generate(user_info)
 
     result = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(response).to be_successful
+    expect(response.status).to eq(201)
     expect(result).to be_a(Hash)
+    expect(result).to have_key(:data)
+    expect(result[:data]).to have_key(:type)
+    expect(result[:data][:type]).to eq("users")
+    expect(result[:data]).to have_key(:id)
+    expect(result[:data][:id]).to be_a(Integer)
+    expect(result[:data]).to have_key(:attributes)
+    expect(result[:data][:attributes]).to be_a(Hash)
+    expect(result[:data][:attributes]).to have_key(:email)
+    expect(result[:data][:attributes][:email]).to be_a(String)
+    expect(result[:data][:attributes]).to have_key(:api_key)
+    expect(result[:data][:attributes][:api_key]).to be_a(String)
   end
 end
