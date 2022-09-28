03121f01d10a34cbf84d59c1bc3b9ac64c5ac1ba
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-28 15:50:28 -0600

    test/Add tests for determining temperature and conditions on arrival

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,9 +1,15 @@
 class RoadTrip
 
-  attr_reader :start_city, :end_city, :travel_time, :temperature, :conditions
+  attr_reader :start_city, :end_city, :trip_time, :temperature, :conditions
   def initialize(start_city, end_city, trip_time, forecast)
     @start_city = start_city
     @end_city = end_city
     @trip_time = Time.at(trip_time).utc.strftime("%H:%M:%S")
+    @conditions = forecast.conditions
+    if forecast.class == DailyWeather
+      @temperature = ((forecast.max_temp + forecast.min_temp) / 2).round(2)
+    else forecast.class == HourlyWeather
+      @temperature = forecast.temp
+    end
   end
 end
