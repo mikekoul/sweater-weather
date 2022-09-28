e4ba4f584134f35632cc96b41fccad5c71854fa3
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-28 12:08:41 -0600

    Add facade method to return the time to destination in seconds

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -25,8 +25,8 @@ RSpec.describe MapService do
       expect(response).to be_a(Hash)
       expect(response).to have_key(:route)
       expect(response[:route]).to be_a(Hash)
-      expect(response[:route]).to have_key(:formattedTime)
-      expect(response[:route][:formattedTime]).to be_a(String)
+      expect(response[:route]).to have_key(:realTime)
+      expect(response[:route][:realTime]).to be_a(Integer)
     end
   end
 end
