46d4fe4b736959be04edc63850541e8fdce1b1d6
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-27 21:18:16 -0600

    feat/Add sessino response that returns API for user

--- spec/requests/api/v1/session_spec.rb
+++ spec/requests/api/v1/session_spec.rb
@@ -21,7 +21,18 @@ describe 'Session API' do
     result = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
-    expect(response.status).to eq(201)
+    expect(response.status).to eq(200)
     expect(result).to be_a(Hash)
+    expect(result).to have_key(:data)
+    expect(result[:data]).to have_key(:type)
+    expect(result[:data][:type]).to eq("users")
+    expect(result[:data]).to have_key(:id)
+    expect(result[:data][:id]).to be_a(Integer)
+    expect(result[:data]).to have_key(:attributes)
+    expect(result[:data][:attributes]).to be_a(Hash)
+    expect(result[:data][:attributes]).to have_key(:email)
+    expect(result[:data][:attributes][:email]).to be_a(String)
+    expect(result[:data][:attributes]).to have_key(:api_key)
+    expect(result[:data][:attributes][:api_key]).to be_a(String)
   end
 end
