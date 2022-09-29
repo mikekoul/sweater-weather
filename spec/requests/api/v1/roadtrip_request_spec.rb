838628fbc265d7811c1c3e59e27049216cb16c7d
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-28 21:07:46 -0600

    test/Add more expect statements to a successful request

--- spec/requests/api/v1/roadtrip_request_spec.rb
+++ spec/requests/api/v1/roadtrip_request_spec.rb
@@ -28,5 +28,24 @@ describe 'RoadTrip API' do
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
+    expect(result).to be_a(Hash)
+    expect(result).to have_key(:data)
+    expect(result[:data]).to have_key(:id)
+    expect(result[:data][:id]).to eq(nil)
+    expect(result[:data]).to have_key(:type)
+    expect(result[:data][:type]).to eq("roadtrip")
+    expect(result[:data]).to have_key(:attributes)
+    expect(result[:data][:attributes]).to have_key(:start_city)
+    expect(result[:data][:attributes][:start_city]).to be_a(String)
+    expect(result[:data][:attributes]).to have_key(:end_city)
+    expect(result[:data][:attributes][:end_city]).to be_a(String)
+    expect(result[:data][:attributes]).to have_key(:travel_time)
+    expect(result[:data][:attributes][:travel_time]).to be_a(String)
+    expect(result[:data][:attributes]).to have_key(:weather_at_eta)
+    expect(result[:data][:attributes][:weather_at_eta]).to be_a(Hash)
+    expect(result[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
+    expect(result[:data][:attributes][:weather_at_eta][:temperature]).to be_a(Float)
+    expect(result[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
+    expect(result[:data][:attributes][:weather_at_eta][:conditions]).to be_a(String)
   end
 end
