0f86ac14a7fe78698b1f3fe37f782b802b7814ff
Michael Koulouvaris <mikekoul@gmail.com>
2022-09-28 20:31:06 -0600

    test/Add test for road trip request and response

--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -5,11 +5,11 @@ class RoadTrip
     @start_city = start_city
     @end_city = end_city
     @trip_time = [trip_time / 3600, trip_time / 60 % 60, trip_time % 60].map{|t| t.to_s.rjust(2,'0')}.join(':')
+    @conditions = forecast.conditions
     if forecast.class == DailyWeather
       @temperature = ((forecast.max_temp + forecast.min_temp) / 2).round(2)
     else forecast.class == HourlyWeather
       @temperature = forecast.temp
     end
-    @conditions = forecast.conditions
   end
 end
---
+++ spec/requests/api/v1/roadtrip_request_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'RoadTrip API' do
+  before :each do
+    @user_info = { 
+      "email": 'mikek@gmail.com',
+      "password": 'pass123',
+      "password_confirmation": 'pass123'
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
+    post '/api/v1/users', headers: headers, params: JSON.generate(@user_info)
+    result = JSON.parse(response.body, symbolize_names: true)
+    @api_key = result[:data][:attributes][:api_key]
+  end
+
+  it 'returns a successfully formatted roadtrip response' do
+    roadtrip_info = {
+      "origin": "Denver,CO",
+      "destination": "Santa Fe, NM",
+      "api_key": "@api_key"
+    } 
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/road_trip', headers: headers, params: JSON.generate(roadtrip_info)
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+  end
+end
